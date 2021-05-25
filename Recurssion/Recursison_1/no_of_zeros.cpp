86daaa130504e6e454ac29cf8b8ebc3541362879
sanjaykazi <51957689+sanjaykazi@users.noreply.github.com>
2021-05-25 19:57:41 +0530

    No of zeros

---
+++ Recurssion/Recursison_1/no_of_zeros.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int zeros(int n){
+  if(n == 0) return 0;
+
+  int small = zeros(n/10);
+
+  int lastDigit = n%10;
+  if(lastDigit == 0 ){
+    return small + 1;
+  }
+  else{
+    return small;
+  }
+}
+int main()
+{
+  int n;
+  cout << "Enter the  two numbers you want to count no of zeros in it: " << endl;
+  cin >>n;
+  cout<<"The number , "<<n<<" has "<<zeros(n)<<" zeros "<<endl;
+  return 0;
+}
