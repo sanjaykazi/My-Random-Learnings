d942d79c9190dba28c9377ea954d7ea08649d447
Sanjay Kazi <51957689+sanjaykazi@users.noreply.github.com>
2021-05-25 14:58:20 +0530

    Update Selection.cpp

--- Selection.cpp
+++ Selection.cpp
@@ -16,37 +16,6 @@ void Sel(vector<int> &vec)
       swap(vec.at(smallest) , vec.at(i));
   }
 }
-void selectionSort(vector<int> &a){
-  int n = a.size();
-
-     /*for(int i=0;i<=n-2;i++){
-
-          int smallest = INT_MAX;
-          for(int j = i+1;j<=n-1;j++){
-            if(a[j]<smallest){
-                smallest = a[j];
-            }
-          }
-
-          if(a[i] > smallest){
-            swap(a[i] , smallest);
-          }
-
-     }*/
-
-
-     for(int i=0;i<=n-2;i++){
-        int smallest = i;
-        for(int j = i+1;j<=n-1;j++){
-
-            if(a.at(j)<a.at(smallest)){
-                smallest = j;
-            }
-        }
-
-        swap(a.at(i),a.at(smallest));
-     }
-}
 
 void print(vector<int> vec)
 {
