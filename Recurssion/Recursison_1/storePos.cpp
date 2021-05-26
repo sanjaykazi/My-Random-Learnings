339e1eb1921ca1c9a1467cb3925d3f368ae5ce12
sanjaykazi <51957689+sanjaykazi@users.noreply.github.com>
2021-05-26 08:36:08 +0530

    storing all the positions of a given element in the array

--- Recurssion/Recursison_1/storePos.cpp
+++ Recurssion/Recursison_1/storePos.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 bool isPresent(int arr[], int n, int key){
   if(n==0){
@@ -42,16 +43,38 @@ bool isPresent4(int arr[], int n, int key,int i){
   }
   return isPresent4(arr,n,key, i+1);
 }
+void saveAllPos(int arr[], int n, int key,  int i, vector<int> &vec){
+  if(i==n){
+    return;
+  }
+  if(arr[i] == key){
+    vec.push_back(i);
+  }
+  saveAllPos(arr,n,key,i+1,vec);
+
+}
+void printVector(vector<int> vec){
+  for(int i=0;i<vec.size();i++){
+    cout<<vec[i]<<" ";
+  }
+  cout<<endl;
+}
 
 int main (){
-  int arr[] = {2,3,1,4,5,9};
-  int n = 6;
-  int key = 9;
-  if(isPresent4(arr, n,key,0)){
+  int arr[] = {2,3,1,4,5,9,3,3};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  vector<int> vec;
+  int key = 3;
+  if(isPresent2(arr, n,key)){
     cout<<key<<" is present in the array."<<endl;
   }else{
     cout<<key<<" is not present in the array."<<endl;
   }
+  saveAllPos(arr,n,key,0,vec);
+  cout<<"The positions of "<<key<<" in the array is/are: "<<endl;
+  printVector(vec);
+
+
 
 
   return 0;
