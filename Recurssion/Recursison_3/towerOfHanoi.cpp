83e6e927078050ab1011ff9931e7a1d9d0d8f154
sanjaykazi <51957689+sanjaykazi@users.noreply.github.com>
2021-05-26 16:16:37 +0530

    printing steps is added in the specification

--- Recurssion/Recursison_3/towerOfHanoi.cpp
+++ Recurssion/Recursison_3/towerOfHanoi.cpp
@@ -9,12 +9,21 @@ int towerOfHanoi(int n){
   }
   return 2*towerOfHanoi(n-1) + 1;
 }
+void toh_step(int n, char s, char h, char d){
+  if(n==0){
+    return;
+  }
+  toh_step(n-1,s,h,d);
+  cout<<"Moving disk "<<n<<" from "<<s<<" to "<< d<<endl;
+  toh_step(n-1,h,d,s);
+}
 int main() 
 {
   int n;
   cout<<"EWnter the no of wheels in hanoi tower game:"<<endl;
   cin>>n;
   cout<<"To move the all discs on to the destination tower we need "<<towerOfHanoi(n)<<" moves."<<endl;
+  toh_step(n,'A','B','C');
   
   
   return 0;
