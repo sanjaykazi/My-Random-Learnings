4d98c93ad7650506fd0b629c8c4b06db17d16608
sanjaykazi1149@gmail.com <sanjaykazi1149@gmail.com>
2021-05-31 16:41:05 +0530

    print functionality added

--- LInked_List/linkedList.cpp
+++ LInked_List/linkedList.cpp
@@ -10,15 +10,38 @@ class Node{
             this->data  = data;
             next = NULL;
         }
-    };
+};
+void print(Node* head){
+    Node* trv = head;
+    while(head !=  NULL){
+        cout<<head->data<<"->";
+        head = head->next;
+    }
+    cout<<"NULL"<<endl;
+}
 int main(){
+    // Nodes created statically
     Node n1(5);
     Node n2(7);
-    n1.next = &n2;
-    cout<<n1.data<<" "<<n2.data<<endl;
-
     Node *head = &n1;
+    Node n3(6);
+    Node n4(1);
+    Node n5(9);
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = &n4;
+    n4.next = &n5;
+    cout<<endl;
+    print(head);
+    // cout<<n1.data<<" "<<n2.data<<endl;
 
+    // Node *head = &n1;
+
+    // //Nodes created dynamically
+    // Node *n3 = new Node(21);
+    // Node *n4 = new Node(49);
+
+    // n3->next = n4;
 
     return 0;
 }
