185ab578a9262fa308bd8fccd7b647d97171ad2d
sanjaykazi1149@gmail.com <sanjaykazi1149@gmail.com>
2021-05-31 18:46:33 +0530

    insertition at first position

--- LInked_List/firstInsert.cpp
+++ LInked_List/firstInsert.cpp
@@ -32,8 +32,8 @@ Node *takeInput(){
             head = n;
             tail = n;
         }else{
-            tail->next = n;
-            tail = n;
+            n->next = head;
+            head = n;
         }
         
         cin>>data;
@@ -44,43 +44,8 @@ Node *takeInput(){
 
 
 int main(){
-    // Nodes created statically
-    Node n1(5);
-    Node n2(7);
-    Node *head = &n1;
-    Node n3(6);
-    Node n4(1);
-    Node n5(9);
-    n1.next = &n2;
-    n2.next = &n3;
-    n3.next = &n4;
-    n4.next = &n5;
-    cout<<endl;
-    print(head);
-    //cout<<endl;
-    // dynamica
-    Node *n6 = new Node(11);
-    Node *n7 = new Node(12);
-    Node *n8 = new Node(13);
-    Node *n9 = new Node(14);
-    Node *n10 = new Node(15);
-
-    n6->next = n7;
-    n7->next = n8;
-    n8->next = n9;
-    n9->next = n10;
-    
     Node *nx = takeInput();
     print(nx);
-    // cout<<n1.data<<" "<<n2.data<<endl;
-
-    // Node *head = &n1;
-
-    // //Nodes created dynamically
-    // Node *n3 = new Node(21);
-    // Node *n4 = new Node(49);
-
-    // n3->next = n4;
 
     return 0;
 }
